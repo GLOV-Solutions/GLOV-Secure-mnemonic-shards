6a9587f378f33b62508f3047cb97c768d4e4fd05
Loulou <loulou@louloucrypto.fr>
2025-11-08 18:28:03 +0100

    Try fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-// vite.config.js (prod simple, pas de single-file)
+// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/',                 // important si tu sers à la racine
+  base: '/', // important si tu sers à la racine (IP/domaine)
   build: {
-    modulePreload: { polyfill: false },
-    assetsInlineLimit: 0,    // n’inline rien
-    cssCodeSplit: true
+    modulePreload: { polyfill: false }, // évite le petit polyfill inline
+    assetsInlineLimit: 0,               // n’inline rien
+    cssCodeSplit: true,                 // CSS dans un fichier
+    rollupOptions: { output: { manualChunks: undefined } }
   }
 });
