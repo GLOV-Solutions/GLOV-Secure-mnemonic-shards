2a999e5db374903cb4dca4f68fc9b98dcbe7b8d0
Guillaume Louchard <guillaume@summit.io>
2026-01-01 15:09:22 +0100

    add minifier

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  base: '/', // important si tu sers à la racine (IP/domaine)
+export default defineConfig(({ mode }) => ({
+  base: '/',
   build: {
-    modulePreload: { polyfill: false }, // évite le petit polyfill inline
-    assetsInlineLimit: 0,               // n’inline rien
-    cssCodeSplit: true,                 // CSS dans un fichier
-    rollupOptions: { output: { manualChunks: undefined } }
+    modulePreload: { polyfill: false },
+    assetsInlineLimit: 0,
+    cssCodeSplit: true,
+    rollupOptions: { output: { manualChunks: undefined } },
+    minify: mode === 'production' ? 'terser' : 'esbuild',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        passes: 2
+      },
+      format: { comments: false }
+    }
   }
-});
+}));
